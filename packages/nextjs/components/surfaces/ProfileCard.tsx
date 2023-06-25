0a5acf4b20c54c163162005083f1f25eeea68dcb
Mel <angel.melendez.c@gmail.com>
2023-06-25 08:52:15 -0400

    Finishing touches

--- packages/nextjs/components/surfaces/ProfileCard.tsx
+++ packages/nextjs/components/surfaces/ProfileCard.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Image from "next/image";
+import { useAccount } from "wagmi";
 
 export type TProfileCardProps = {
   actions?: any;
   role: string;
 };
 
-const ProfileCard = ({ actions, role }: TProfileCardProps) => {
+const ProfileCard = ({ role }: TProfileCardProps) => {
+  const { address } = useAccount();
   let imgPath: string;
   switch (role) {
     case "Project":
@@ -21,6 +24,20 @@ const ProfileCard = ({ actions, role }: TProfileCardProps) => {
     default:
       imgPath = "/assets/avatars/projectRoleAvatar.png";
   }
+
+  const handleValidateStake = async () => {
+    try {
+      const res = fetch(`http://localhost:3000/api/unlock?address=${address}`, {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="overflow-hidden rounded-md bg-white w-full h-full pb-2 ring-4 ring-red-600">
       <div className="pt-4 flex justify-center">
@@ -28,7 +45,7 @@ const ProfileCard = ({ actions, role }: TProfileCardProps) => {
           <button
             type="button"
             className="rounded-lg bg-red-600 px-8 py-3 text-lg font-semibold text-white ring-1 ring-inset ring-gray-300 hover:bg-red-500"
-            onClick={() => actions.nextStep({ type: "role", payload: role })}
+            onClick={handleValidateStake}
           >
             Validate Stake
           </button>
